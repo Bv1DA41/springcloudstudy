27224a625a35ab975470ddd2873e505549e9dbba
xiaomai <xiaomai@163.com>
2021-07-17 15:15:52 +0800

    7.17-15:15

---
+++ springcloud-eurkea-7002/src/main/java/com/kuang/springcloud/EurekaServer_7002.java
@@ -0,0 +1,21 @@
+package com.kuang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @BelongsProject: springcloud
+ * @BelongsPackage: com.kuang.springcloud
+ * @Author: cxy
+ * @Date: 2021/7/17 12:37
+ * @Description: TODO
+ */
+@SpringBootApplication
+//EnableEurekaServer服务端的启动类，可以接受别人注册进来
+@EnableEurekaServer
+public class EurekaServer_7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7002.class, args);
+    }
+}
